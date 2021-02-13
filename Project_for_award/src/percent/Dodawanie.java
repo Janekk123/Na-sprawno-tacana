c077d6381b923f8359a17c1bfe6bcebe36c26cb4
Janekk123 <71390629+Janekk123@users.noreply.github.com>
2021-02-13 22:36:21 +0100

    Add files via upload

---
+++ Project_for_award/src/percent/Dodawanie.java
@@ -0,0 +1,12 @@
+package percent;
+
+public class Dodawanie {
+	public static void main(String[] args) {
+	int a = 119,b = 231;
+	System.out.println("Dodamy liczby 100+231");
+	int wynik = a+b;
+	System.out.print("wynik to ");System.out.print(wynik);  
+	
+
+	}
+	}
